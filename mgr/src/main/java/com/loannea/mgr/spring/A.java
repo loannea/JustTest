3286d854cca9e7c8896d88bb815b2eb4831a14f5
zhujunwen <931078457@qq.com>
2020-05-12 09:56:28 +0800

    feat(module): thread & sring beanCurrentlyCreationTest && fork

---
+++ mgr/src/main/java/com/loannea/mgr/spring/A.java
@@ -0,0 +1,12 @@
+package com.loannea.mgr.spring;
+
+import lombok.Data;
+
+/**
+ * {@code A} TODO()
+ * @author zjw
+ */
+@Data
+public class A {
+    private B b;
+}
---
+++ mgr/src/main/java/com/loannea/mgr/spring/B.java
@@ -0,0 +1,12 @@
+package com.loannea.mgr.spring;
+
+import lombok.Data;
+
+/**
+ * {@code B} TODO()
+ * @author zjw
+ */
+@Data
+public class B {
+    private A a;
+}
